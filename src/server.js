024dbea74754d161e4cf05cb41eafc4aa50fc401
gvinianidzegivi <gvinianidzegivi@gmail.com>
2023-04-14 13:59:44 +0400

    project setup

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
